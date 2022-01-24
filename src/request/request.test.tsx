9244d01cfda7a859b01f580abd7bca8b05abc190
felippe-regazio <felippe.moraes@zoho.com>
2022-01-24 10:41:09 -0300

    type fix

--- src/request/request.test.tsx
+++ src/request/request.test.tsx
@@ -1,7 +1,7 @@
 import { getNeoWsFeed } from './nasa'
 
-describe('Check API helpers (request)', done => {
-  test('The getNeoWsFeed API function must return the proper data ', done => {
+describe('Check API helpers (request)', () => {
+  test('The getNeoWsFeed API function must return the proper data ', (done: any) => {
     getNeoWsFeed().then(data => {
       expect(data.element_count > 0).toBeTruthy();
       
